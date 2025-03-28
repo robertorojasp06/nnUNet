51207fd7d10f0f578f997fdd92e32e2d19e0bed8
Roberto Rojas <roberto.rojasp06@gmail.com>
2025-03-28 14:18:30 -0300

    Add custom Trainer for finetuning.

---
+++ nnunetv2/training/nnUNetTrainer/variants/hcuch/finetuning.py
@@ -0,0 +1,13 @@
+import torch
+
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainer_lr_0_001_300epochs_every20(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int,
+                 dataset_json: dict, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.initial_lr = 1e-3
+        self.num_epochs = 300
+        self.save_every = 20
+
