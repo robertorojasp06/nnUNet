a862d15d51bebea453b5b93b1fd7bf2abbe87891
Roberto Rojas <roberto.rojasp06@gmail.com>
2025-03-29 12:33:39 -0300

    Add new Trainer.

--- nnunetv2/training/nnUNetTrainer/variants/hcuch/finetuning.py
+++ nnunetv2/training/nnUNetTrainer/variants/hcuch/finetuning.py
@@ -11,3 +11,12 @@ class nnUNetTrainer_lr_0_001_300epochs_every20(nnUNetTrainer):
         self.num_epochs = 300
         self.save_every = 20
 
+
+class nnUNetTrainer_lr_0_0001_300epochs_every20(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int,
+                 dataset_json: dict, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.initial_lr = 1e-4
+        self.num_epochs = 300
+        self.save_every = 20
+
